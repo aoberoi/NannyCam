84aaf0baf1e0466e377b29d14ed9adea32589e87
Ankur Oberoi <aoberoi@gmail.com>
2013-01-12 11:18:11 -0500

    explicitly states hostname

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ var routes = require('./routes')(app);
 /*
  * Server
  */
-http.createServer(app).listen(app.get('port'), function(){
+http.createServer(app).listen(app.get('port'), 'localhost', function(){
   console.log("Express server listening on port " + app.get('port'));
 });
 
